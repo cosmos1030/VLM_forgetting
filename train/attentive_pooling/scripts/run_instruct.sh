8d6f3b2b5425751af5c420f14cab2ff8c86b5d87
cosmos1030 <dyk6208@gmail.com>
2025-08-15 11:02:48 +0900

    chore: add script

---
+++ train/attentive_pooling/scripts/run_instruct.sh
@@ -0,0 +1,19 @@
+python attentive_pooling_instruct.py \
+    --dataset CIFAR100 \
+    --model_id /public/home/group_ucb/dyk6208/LLaMA-Factory/saves/qwen2_5_vl-3b/full/cifar10_sft/checkpoint-31250 \
+    --cuda_device 0
+
+python attentive_pooling_instruct.py \
+    --dataset CIFAR10 \
+    --model_id /public/home/group_ucb/dyk6208/LLaMA-Factory/saves/qwen2_5_vl-3b/full/cifar100_sft/checkpoint-31250 \
+    --cuda_device 0
+
+python attentive_pooling_instruct.py \
+    --dataset CIFAR10 \
+    --model_id /public/home/group_ucb/dyk6208/VLM_forgetting/train/fully_finetuning_grpo/checkpoints/GRPO-Qwen2.5-VL-3B-Instruct-cifar100/checkpoint-7200 \
+    --cuda_device 0
+
+python attentive_pooling_instruct.py \
+    --dataset CIFAR100 \
+    --model_id /public/home/group_ucb/dyk6208/VLM_forgetting/train/fully_finetuning_grpo/checkpoints/GRPO-Qwen2.5-VL-3B-Instruct-cifar10/checkpoint-7200 \
+    --cuda_device 0
