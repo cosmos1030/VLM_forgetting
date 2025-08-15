76d8c84c84267b39560513e5ffb79ceb5427a5b3
cosmos1030 <dyk6208@gmail.com>
2025-08-15 11:14:31 +0900

    minor change for server

--- train/attentive_pooling/scripts/run_instruct.sh
+++ train/attentive_pooling/scripts/run_instruct.sh
@@ -1,5 +1,15 @@
 python attentive_pooling_instruct.py \
     --dataset CIFAR100 \
+    --model_id /public/home/group_ucb/dyk6208/Qwen2.5-VL-3B-Instruct \
+    --cuda_device 0
+
+python attentive_pooling_instruct.py \
+    --dataset CIFAR10 \
+    --model_id /public/home/group_ucb/dyk6208/Qwen2.5-VL-3B-Instruct \
+    --cuda_device 0
+
+python attentive_pooling_instruct.py \
+    --dataset CIFAR100 \
     --model_id /public/home/group_ucb/dyk6208/LLaMA-Factory/saves/qwen2_5_vl-3b/full/cifar10_sft/checkpoint-31250 \
     --cuda_device 0
 
